5c28126479e7562869b6a1ae91a68f76b608fdb0
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-01 12:27:01 +0200

    feat: add order reducer base logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useReducer } from "react"
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
+import orderReducer, { initialState } from "./reducers/orderReducer"
 
 function App() {
 
   const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
+  const [state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
--- src/reducers/orderReducer.ts
+++ src/reducers/orderReducer.ts
@@ -1,7 +1,37 @@
-import { MenuItem } from "../types"
+import { MenuItem, OrderItem } from "../types"
 
 export type OrderActions =
     { type: 'add-item', payload: { item: MenuItem } } |
     { type: 'remove-item', payload: { id: MenuItem['id'] } } |
     { type: 'place-order', payload: {} } |
     { type: 'add-tip', payload: { value: number } }
+
+export type OrderState = {
+    order: OrderItem[],
+    tip: number
+}
+
+export const initialState: OrderState = {
+    order: [],
+    tip: 0
+}
+
+export default (state = initialState, { type, payload }: OrderActions) => {
+  switch (type) {
+
+  case 'add-item':
+    return { ...state, ...payload }
+
+  case 'remove-item':
+    return { ...state, ...payload }
+
+  case 'place-order':
+    return { ...state, ...payload }
+
+  case 'add-tip':
+    return { ...state, ...payload }
+
+  default:
+    return state
+  }
+}
