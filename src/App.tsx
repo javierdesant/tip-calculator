107a3bc8722401844e9a9cbe66c092fb55c5618e
JavierDeSant <j.desantiagosoto@gmail.com>
2024-09-01 12:48:49 +0200

    fix: fix order content and calculator bug

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,10 @@ function App() {
       </div>
 
       <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-        {order.length ? (
+        {state.order.length ? (
           <>
             <OrderContents
-              order={order}
+              order={state.order}
               removeItem={removeItem}
             />
     
@@ -48,7 +48,7 @@ function App() {
             />
     
             <OrderTotals 
-              order={order}
+              order={state.order}
               tip={tip}
               placeOrder={placeOrder}
             />
